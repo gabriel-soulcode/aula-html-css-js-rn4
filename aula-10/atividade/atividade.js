1e55a80680185c5485109ff1127c401556e2aae1
gab-braga <gabrielbraga0712@gmail.com>
2025-01-21 09:42:16 -0300

    aula 10 atividade

---
+++ aula-10/atividade/atividade.js
@@ -0,0 +1,47 @@
+// 1. Declare três variáveis chamadas nome, idade e altura. Atribua valores apropriados a cada uma delas.
+let nome = "Rafael";
+let idade = 30;
+let altura = 1.85;
+console.log("Nome:", nome);
+console.log("Idade:", idade);
+console.log("Altura:", altura);
+
+// 2. Declare uma variável estudante e atribua a ela um valor booleano. Explique o que este valor representa.
+let estudante = false;
+console.log("É um estudante?", estudante);
+
+// 3. Declare duas variáveis a e b com valores 10 e 5, respectivamente. Calcule e exiba no console:
+let a = 10, b = 5;
+// A soma de a e b.
+let soma = a + b;
+console.log("Adição", soma);
+// A diferença entre a e b.
+console.log("Subtração", a - b);
+// O produto de a e b.
+console.log("Mutiplicação", a * b);
+// O quociente de a dividido por b.
+console.log("Divisão", a / b);
+
+// 4. Dada a variável contador com valor inicial de 0:
+let contador = 0;
+console.log("Inicio", contador);
+// Aplique o operador de incremento para adicionar 1.
+console.log("Incremento", ++contador);
+// Aplique o operador de decremento para subtrair 1.
+console.log("Decremento", --contador);
+
+// 5. Declare uma variável total com valor inicial 100. Use o operador de atribuição para:
+let total = 100;
+console.log("Inicio", total);
+// Adicionar 50 a total.
+total += 50;
+console.log(total);
+// Subtrair 20 de total.
+total -= 20;
+console.log(total);
+// Multiplicar total por 2.
+total *= 2;
+console.log(total);
+// Dividir total por 5.
+total /= 5;
+console.log(total);
